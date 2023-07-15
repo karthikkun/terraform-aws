413bf1cfd127946c47ebf0eab06e4cad67eef49c
karthik <karthik>
2023-07-15 17:20:08 -0400

    security group inbound and outbound rules

--- ec2/main.tf
+++ ec2/main.tf
@@ -12,8 +12,63 @@ provider "aws" {
   profile = "default"
 }
 
-resource "aws_instance" "web" {
+resource "aws_security_group" "web-terraform-sg" {
+  name = "web-terraform-sg"
+  description = "web-terraform-sg-description"
+  # vpc_id = ""
+
+  // ssh access
+  # ingress {
+  #   from_port = 22
+  #   protocol = "tcp"
+  #   to_port = 22
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
+
+  ingress {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_instance" "web-terraform-instance" {
   ami           = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
   instance_type = "t2.micro"
   count         = 1
+  associate_public_ip_address = true
+
+  vpc_security_group_ids = [
+    aws_security_group.web-terraform-sg.id
+  ]
+
+  root_block_device {
+    delete_on_termination = true
+    volume_type = "gp2"
+  }
+
+  tags = {
+    Name ="web-terraform-instance"
+    Environment = "SANDBOX"
+    OS = "UBUNTU"
+    Managed = "IAC"
+  }
+
+  depends_on = [ aws_security_group.web-terraform-sg ]
+}
+
+output "ec2instance-public-ip" {
+  value = aws_instance.web-terraform-instance[0].public_ip
 }
